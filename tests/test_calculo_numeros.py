e7d6ed985864c86cf271f4dc72fffc7265246fa5
Joaquín <kiki0511@example.com>
2025-04-14 19:16:13 -0300

    Implementación de pruebas para validación de números, en el caso que las entradas sean negativas

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -8,3 +8,9 @@ class TestCalculoNumeros(unittest.TestCase):
         # Caso en el que la entrada es un número positivo válido
         numero = ingrese_numero()
         self.assertEqual(numero, 100)
+
+    @patch('builtins.input', return_value='-100')
+    def test_ingreso_negativo(self, patch_input):
+        # Caso en el que la entrada es negativa; se espera que se lance la excepción personalizada
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
